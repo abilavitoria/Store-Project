dd7f27197030ee93f4e836abab83999e94510671
abilavitoria <toriaraujo6@gmail.com>
2026-03-15 09:22:00 -0300

    feat: adicionei alguns comentarios a tabela vendas

--- vendas_table.sql
+++ vendas_table.sql
@@ -1,12 +1,11 @@
-
 /*tabela vendas*/
 CREATE TABLE vendas.vendas(
-	id serial PRIMARY KEY,/*serial autoincr*/
+	id serial PRIMARY KEY,
 	cliente_id INT NOT NULL,
 	produto_id INT NOT NULL,
 	descricao_venda TEXT,
 	preco_total DECIMAL(10,2) NOT NULL,
-	data_venda TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	data_venda TIMESTAMP DEFAULT CURRENT_TIMESTAMP,/*dd/mm/aaaa/h:m:s*/
 
 	/*criando os relacionamentos*/
 	CONSTRAINT fk_cliente FOREIGN KEY (cliente_id) REFERENCES comercial.clientes(id),
