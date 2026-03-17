0d5b4eb624a3a3e4d2610974f0cc5dfbd38f3044
abilavitoria <toriaraujo6@gmail.com>
2026-03-17 18:44:08 -0300

    feat:criei no controller as classes para clientes, produtos e vendas

---
+++ Store/src/main/java/com/abila/Store/controller/ProdutosController.java
@@ -0,0 +1,9 @@
+package com.abila.Store.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutosController {
+}
