c62b63b39bb0410cf972df32aafe9276b4e5a1c9
abilavitoria <toriaraujo6@gmail.com>
2026-03-23 16:47:27 -0300

    feat: domain das classes de clientes e produtos

--- Store/src/main/java/com/abila/Store/domain/Clientes.java
+++ Store/src/main/java/com/abila/Store/domain/Clientes.java
@@ -1,4 +1,28 @@
 package com.abila.Store.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table
 public class Clientes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 100, nullable = false)
+    private String nome;
+    @Column(length = 100, nullable = false)
+    private String email;
+    @Column(length = 11, nullable = false)
+    private String cpf;
+    @Column(length = 14, nullable = false)
+    private String cnpj;
 }
--- Store/src/main/java/com/abila/Store/domain/Produtos.java
+++ Store/src/main/java/com/abila/Store/domain/Produtos.java
@@ -1,4 +1,29 @@
 package com.abila.Store.domain;
 
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table
 public class Produtos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    @Column(length = 100, nullable = false)
+    private String nome;
+    @Column(length = 100, nullable = false)
+    private String descricao;
+    @Column(nullable = false)
+    private Float preco;
+    @Column(nullable = false)
+    private Integer quantidade;
 }
