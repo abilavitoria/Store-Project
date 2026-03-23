bd42c8509b1585be727c20d72b2ab5cc99a7a3a1
abilavitoria <toriaraujo6@gmail.com>
2026-03-23 18:34:44 -0300

    feat:domain vendas

--- Store/src/main/java/com/abila/Store/domain/Vendas.java
+++ Store/src/main/java/com/abila/Store/domain/Vendas.java
@@ -1,4 +1,32 @@
 package com.abila.Store.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table
 public class Vendas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private Integer clienteId;
+    @Column(nullable = false)
+    private Integer produto_Id;
+    @Column(length = 100)
+    private String descricao;
+    @Column(nullable = false)
+    private Double precoTotal;
+    @Column(nullable = false)
+    private Date data;
 }
