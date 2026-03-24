8c4d0bf09a2799e0994010cea65e59555e3a1da0
abilavitoria <toriaraujo6@gmail.com>
2026-03-24 16:30:25 -0300

    feat:ajustei parte do controller clientes

--- Store/src/main/java/com/abila/Store/controller/ClientesController.java
+++ Store/src/main/java/com/abila/Store/controller/ClientesController.java
@@ -1,6 +1,12 @@
 package com.abila.Store.controller;
 
+<<<<<<< Updated upstream
 import com.abila.Store.domain.Clientes;
+=======
+import com.abila.Store.Service.ClienteService;
+import com.abila.Store.domain.Clientes;
+import com.abila.Store.repository.ClienteRepository;
+>>>>>>> Stashed changes
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +18,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/clientes")
 public class ClientesController {
+<<<<<<< Updated upstream
     private final ClientesController clientesController;
     //consultar
     @GetMapping
@@ -31,5 +38,33 @@ public class ClientesController {
     public Clientes update(@PathVariable Integer id, @RequestBody Clientes clientes){
         clientes.setId(id);
         return clienteRepo.save(clientes);
+=======
+    private final ClienteService clienteService;
+    //consultar
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Clientes>> consultaClientes(@PathVariable Integer id){
+        return ResponseEntity.ok(clienteService.findById(id));
+    }
+
+    //cadastrar
+    @PostMapping
+    public ResponseEntity<Clientes> salvarClientes(@RequestBody Clientes clientes){
+        Clientes savedClientes = clienteService.saveClientes(clientes);
+        return ResponseEntity.status(201).body(savedClientes);
+    }
+
+    //excluir
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCliente(@PathVariable Integer id){
+        clienteService.deleteClientes(id);
+        return ResponseEntity.status(204).build();
+    }
+
+    //editar
+    @PutMapping("/{id}")
+    public ResponseEntity<Clientes> updateClientes(@RequestBody Clientes clientes, @PathVariable Integer id){
+        clientes.setId(id);
+        return ResponseEntity.ok(clienteService.saveClientes(clientes));
+>>>>>>> Stashed changes
     }
 }
