7e97163a4ad625c705f73acccb4c09addac9783a
abilavitoria <toriaraujo6@gmail.com>
2026-03-25 16:35:20 -0300

    feat:previa vendas controller e service

---
+++ Store/src/main/java/com/abila/Store/Service/VendasService.java
@@ -0,0 +1,45 @@
+package com.abila.Store.Service;
+
+import com.abila.Store.domain.Vendas;
+import com.abila.Store.repository.VendasRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class VendasService {
+    private final VendasRepository vendasRepo;
+
+    //consultar
+    public Optional<Vendas> findById(Integer id){
+        return vendasRepo.findById(id);
+    }
+    //salvar
+    public Vendas saveVendas(Vendas vendas){
+        return vendasRepo.save(vendas);
+    }
+    //excluir
+    public void deleteVendas(Integer id){
+        if (!vendasRepo.existsById(id)){
+            throw new RuntimeException("Venda não encontrada");
+        }
+        else {
+            vendasRepo.deleteById(id);
+        }
+    }
+    //editar
+    public Vendas updateVendas(Vendas vendas, Integer id){
+        return vendasRepo.findById(id)
+                .map(existisVendas ->{
+                    vendas.setId(id);
+                    return vendasRepo.save(vendas);
+                })
+                .orElseThrow(() -> new RuntimeException("Venda com id" + id + "não encontrada"));
+    }
+
+    //adicionar itens
+    //remover itens
+    //editar itens
+}
--- Store/src/main/java/com/abila/Store/controller/VendasController.java
+++ Store/src/main/java/com/abila/Store/controller/VendasController.java
@@ -1,9 +1,41 @@
 package com.abila.Store.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.abila.Store.Service.VendasService;
+import com.abila.Store.domain.Vendas;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
 
 @RestController
 @RequestMapping("/vendas")
 public class VendasController {
+    private final VendasService vendasService;
+    //consultar
+    @GetMapping("/{id}")
+    public ResponseEntity<Vendas> findVendasById(@PathVariable Integer id){
+        return vendasService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    //salvar
+    @PostMapping
+    public ResponseEntity<Vendas> save(@RequestBody Vendas vendas){
+        Vendas novaVenda = vendasService.saveVendas(vendas);
+        return ResponseEntity.status(201).body(novaVenda);
+    }
+    //excluir
+    @DeleteMapping("/{}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        vendasService.deleteVendas(id);
+        return ResponseEntity.noContent().build();
+    }
+    //editar
+    
+
+    //adicionar itens
+    //remover itens
+    //editar itens
 }
