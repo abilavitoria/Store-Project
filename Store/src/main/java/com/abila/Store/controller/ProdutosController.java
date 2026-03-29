5aef045fc152d6a1039c13d9f2a967ef6aed6c47
abilavitoria <toriaraujo6@gmail.com>
2026-03-28 23:13:32 -0300

    feat: ajustes de autorização dos controllers

--- Store/src/main/java/com/abila/Store/controller/ProdutosController.java
+++ Store/src/main/java/com/abila/Store/controller/ProdutosController.java
@@ -4,12 +4,10 @@ import com.abila.Store.service.ProdutoService;
 import com.abila.Store.domain.Produtos;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RequiredArgsConstructor
-
 @RestController
 @RequestMapping("/produtos")
 public class ProdutosController {
@@ -17,6 +15,7 @@ public class ProdutosController {
 
     //consultar
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     public ResponseEntity<Produtos> findProdutosById(@PathVariable Integer id){
         return produtoService.findById(id)
                 .map(ResponseEntity::ok)
@@ -24,11 +23,13 @@ public class ProdutosController {
     }
     //cadastrar
     @PostMapping
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     public ResponseEntity<Produtos> save(@RequestBody Produtos produtos){
         Produtos novoProduto = produtoService.saveProdutos(produtos);
         return ResponseEntity.status(201).body(novoProduto);
     }
     //excluir
+    @PreAuthorize("hasAuthority('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id){
         produtoService.deleteProdutos(id);
@@ -37,6 +38,7 @@ public class ProdutosController {
 
     //editar
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     public ResponseEntity<Produtos> update(@PathVariable Integer id, @RequestBody Produtos produtos){
         return ResponseEntity.ok(produtoService.updateProdutos(id, produtos));
     }
--- Store/src/main/java/com/abila/Store/controller/VendasController.java
+++ Store/src/main/java/com/abila/Store/controller/VendasController.java
@@ -5,12 +5,11 @@ import com.abila.Store.service.VendasService;
 import com.abila.Store.domain.Vendas;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 
-@RestController
 @RequestMapping("/vendas")
 public class VendasController {
     private final VendasService vendasService;
@@ -18,6 +17,7 @@ public class VendasController {
     //                  METODOS DE VENDA
     //consultar
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     public ResponseEntity<Vendas> findVendasById(@PathVariable Integer id){
         return vendasService.findById(id)
                 .map(ResponseEntity::ok)
@@ -25,18 +25,21 @@ public class VendasController {
     }
     //salvar
     @PostMapping
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     public ResponseEntity<Vendas> save(@RequestBody Vendas vendas){
         Vendas novaVenda = vendasService.saveVendas(vendas);
         return ResponseEntity.status(201).body(novaVenda);
     }
     //excluir
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Void> delete(@PathVariable Integer id){
         vendasService.deleteVendas(id);
         return ResponseEntity.noContent().build();
     }
     //editar
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     public ResponseEntity<Vendas> update(@RequestBody Vendas vendas, @PathVariable Integer id){
         return ResponseEntity.ok(vendasService.updateVendas(vendas, id));
     }
@@ -44,17 +47,20 @@ public class VendasController {
     //                  METODOS DE ITEM
     //adicionar itens
     @PostMapping("/{vendaId}/itens")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     public ResponseEntity<ItemVendas> addItem(@PathVariable Integer vendaId, @RequestBody ItemVendas item){
         ItemVendas novoItem = vendasService.addItemVendas(vendaId, item);
         return ResponseEntity.status(201).body(novoItem);
     }
     //editar itens
     @PutMapping("/itens/{itemId}")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     public ResponseEntity<ItemVendas> updateItem(@PathVariable Integer itemId, @RequestBody ItemVendas item){
         return ResponseEntity.ok(vendasService.updateItemVendas(itemId, item));
     }
     //delete itens
     @DeleteMapping("/iten/{itemId}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Void> deleteItem(@PathVariable Integer itemId){
         vendasService.removeItemVendas(itemId);
         return ResponseEntity.noContent().build();
