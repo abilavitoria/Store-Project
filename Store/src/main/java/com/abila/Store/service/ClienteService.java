34e0d03d94984537be864aaa51180f2b6323265b
abilavitoria <toriaraujo6@gmail.com>
2026-03-30 17:01:11 -0300

    feat: logica para autorizacao de cpf:cliente

--- Store/src/main/java/com/abila/Store/service/ClienteService.java
+++ Store/src/main/java/com/abila/Store/service/ClienteService.java
@@ -4,6 +4,7 @@ import com.abila.Store.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.*;
 import java.util.Optional;
 
 @Service
@@ -18,6 +19,9 @@ public class ClienteService {
 
     //cadastrar
     public Clientes saveClientes(Clientes clientes){
+        if(!validacaoCpf(clientes.getCpf())){
+            throw new RuntimeException("CPF informado não é valido!");
+        }
         return clienteRepo.save(clientes);
     }
 
@@ -37,6 +41,48 @@ public class ClienteService {
                     return clienteRepo.save(clientes);
                })
                .orElseThrow(()-> new RuntimeException("Cliente com id" + id + "não encontrado"));
+    }
+
+    //FUNCOES
+    public boolean validacaoCpf(String cpf){
+        cpf = cpf.replaceAll("\\D", "");
+
+        if(cpf.length() != 11 || cpf.matches("(\\d)\\1{10}")) return false;
+
+        int[] numeros = new int[11];
+        for(int i = 0; i < 11; i++){
+            numeros[i] = Character.getNumericValue(cpf.charAt(i));
+        }
+
+        //VERIFICACAO PENULTIMO DIGITO
+        int soma1 = 0;
+        int peso1 = 10;
+
+        for (int i = 0; i < 9; i++){
+            soma1 += numeros[i] * peso1;
+            peso1--;
+        }
+
+        int resto1 = soma1 % 11;
+        int penultimo = (resto1 < 2) ? 0 : 11 - resto1;
+
+        //VERIFICACAO ULTIMO DIGITO
+        int soma2 = 0;
+        int peso2 = 11;
+         for(int i = 0; i < 10; i++){
+             soma2 += numeros[i] * peso2;
+             peso2--;
+         }
+
+         int resto2 = soma2 % 11;
+         int ultimo = (resto2 < 2 ) ? 0: 11 - resto2;
+
+         if(penultimo == numeros[9] && ultimo == numeros[10]){
+             return true;
+         }
+         else {
+             return false;
+         }
     }
 
 }
