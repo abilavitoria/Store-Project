b84267e75151040b30a28f852e2b13140608fefd
abilavitoria <toriaraujo6@gmail.com>
2026-04-13 19:05:31 -0300

    fix: correcoes finais

---
+++ Store/src/main/java/com/abila/Store/controller/ClientesController.java
@@ -0,0 +1,35 @@
+package com.abila.Store.controller;
+
+import com.abila.Store.domain.Clientes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+
+@RestController
+@RequestMapping("/clientes")
+public class ClientesController {
+    private final ClientesController clientesController;
+    //consultar
+    @GetMapping
+    public ResponseEntity<Clientes> findClienteById(@PathVariable Integer id){
+        Clientes
+        return ResponseEntity.status(201).body()
+    }
+    //salvar
+    public Clientes save(@RequestBody Clientes clientes){
+        return clienteRepo.save(clientes);
+    }
+    //excluir
+    public void delete(@PathVariable Integer id){
+        clienteRepo.deleteById(id);
+    }
+    //editar
+    public Clientes update(@PathVariable Integer id, @RequestBody Clientes clientes){
+        clientes.setId(id);
+        return clienteRepo.save(clientes);
+    }
+}
--- back-end/src/main/java/com/abila/Store/domain/DTO/VendaRequest.java
+++ back-end/src/main/java/com/abila/Store/domain/DTO/VendaRequest.java
@@ -3,6 +3,7 @@ package com.abila.Store.domain.DTO;
 import com.abila.Store.domain.Clientes;
 import com.abila.Store.domain.ItemVendas;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
@@ -11,8 +12,7 @@ import java.util.List;
 
 public record VendaRequest(
         String descricao,
-        @NotBlank @Positive BigDecimal precoTotal,
-        @NotBlank LocalDateTime data,
+        @NotNull @Positive BigDecimal precoTotal,
         Clientes clientes,
         List<ItemVendas> itens
 ) {
